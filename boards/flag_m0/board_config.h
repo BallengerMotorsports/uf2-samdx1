44befbc53d04de640b25e7155185561db2f2c6ad
James Ballenger <vtjballeng@gmail.com>
2022-01-14 17:26:48 -0500

    Added generic bootloader for devices other than handheld controller & signboards. Applied in Flagtronics-Arduino repo.

--- boards/flag_m0/board_config.h
+++ boards/flag_m0/board_config.h
@@ -1,13 +1,13 @@
 #ifndef BOARD_CONFIG_H
 #define BOARD_CONFIG_H
 
-#define VENDOR_NAME "BMSP"
-#define PRODUCT_NAME "FT-Device"
-#define VOLUME_LABEL "FT-Device"
-#define INDEX_URL "http://flagtronics.com/"
+#define VENDOR_NAME "Flagtronics"
+#define PRODUCT_NAME "Flagtronics M0"
+#define VOLUME_LABEL "FT-DeviceBL"
+#define INDEX_URL "https://flagtronics.com/"
 #define BOARD_ID "Flagtronics-Controller-v0"
 
 #define USB_VID 0x04D8
-#define USB_PID 0xE86B
+#define USB_PID 0xE7F3
 
 #endif
