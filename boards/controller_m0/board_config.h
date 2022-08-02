6b3ae2ec7ea069b884a0adc66e41604c37c9fe28
mfvmx <morganf83@yahoo.com>
2022-08-02 13:15:35 -0400

    Update version on Controller BL v0.1

--- boards/controller_m0/board_config.h
+++ boards/controller_m0/board_config.h
@@ -5,10 +5,10 @@
 #define PRODUCT_NAME "Controller M0"
 #define VOLUME_LABEL "FT-Cont-BL"
 #define INDEX_URL "https://flagtronics.com/"
-#define BOARD_ID "Controller-v0"
+#define BOARD_ID "Controller-v0.1"
 
 #define USB_VID 0x04D8
-#define USB_PID 0x0831
+#define USB_PID 0xE7F3
 
 #define NO_DBL_TAP_BOOT
 #define PWR_PIN PIN_PA17
